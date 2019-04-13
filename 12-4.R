d4242a2d2edf0dce7fc9796b79476c4681c58eb9
angela <angelalaramoreira@gmail.com>
2019-04-13 11:07:46 -0400

    clase 5

--- 12-4.R
+++ 12-4.R
@@ -1,5 +1,45 @@
-funcionuno<-function(){
+funcionuno<-function(x,y){
   #aquiva el desarrollo de la funcion
-  print("hola mundo")
+  print(x+y)
+
+  print("ahora voy a sumar")
+  
+  print("entro a la suma")
+  }
+
+#realizar el desarrollo de la velocidad de un cuerpo en relacion al tiempo
+# function velocidad ( double,double)->double
+
+#test velocidad(10,2)->5
+
+funcionVelocidad<-function(d,t){
+  
+    print(d/t)
+}
+#test de velocidad
+
+#funcionresto
+
+#realizar una funcion que calcule el resto de la multiplicacion de 2 numeros
+
+#respecto un tercero
+#funcion resto (int,int,int)->int
+#resto(3,4,7)->5
+
+funcionResto<-function(x,y,z){
+  (x*y)%%z
 }
 
+algebra<-fuction(a,b,c){
+  trunc((a*b)-(((a*b)/c)b*))
+  
+  
+}
+  
+
+
+  
+  
+  
+  
+  
--- 12-42.R
+++ 12-42.R
@@ -1,2 +1,8 @@
 source("12-4.R")
-funcionuno()
+variableuno<-funcionuno(1,2)
+funcionuno(1,2)
+vel<-velocidad(10,5)
+
+funcionVelocidad(10,2)
+
+funcionResto(3,4,7)
