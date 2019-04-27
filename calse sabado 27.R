14bd4d540e463134f301587e84376a8d9ac55f18
angela <angelalaramoreira@gmail.com>
2019-04-27 11:23:49 -0400

    clase 26-27

---
+++ calse sabado 27.R
@@ -0,0 +1,91 @@
+if(true){
+  print("entro al if")
+}
+if(FALSE){
+  print("entro al if")
+}
+if(!FALSE
+   ){
+  print("entro al if")
+}
+if(2+2==4){
+  print("entro al if")
+}
+
+
+if(2+2==4 || 3+3==5){
+  print("entreo al if")
+}
+
+if(2+2==4 && 3+3==5){
+  print("entreo al if")
+}
+
+if(TRUE){
+  print("entro al if")
+}else{
+   print("entro al else")
+}
+
+
+if(FALSE){
+  print("entro al if")
+}else{
+print("entro al else")}
+
+anillo<-function(R,r){3.1415(R^2-r^2)}
+anillo2<-function(R,r){areaCirculo(R)-areaCirculo(r)}
+
+if(R>r){3.14*(R^2-r^2)
+}else{
+    (r>R){3.14*(r^2-R^2)}
+  }
+  
+if(radio1>radio2){
+  anillo2(radio1,radio2)
+}else{
+  anillo2(radio2,radio1)
+}
+source(areaCirculo(R)
+
+       if(radio1>radio2){
+         areaCirculo(radio1)-areaCirculo(radio2)
+         }else{
+           areaCirculo(radio2)-areaCirculo(radio1)
+       }
+  
+algo<-function(x){
+  if(x>0){
+    print ("mayor a cero")
+  }else if (x<0){
+    print("menor a cero")
+  }else if(x==0){
+   print("distinto de cero")
+  }
+}
+
+
+
+algo<-function(x){
+  if(x>0){
+    print ("mayor a cero")
+  }else if (x<0){
+    print("menor a cero")
+  }else {"otr"
+    
+  }
+}
+
+
+
+
+if(x>0){
+  print("mayor a cero")
+}else if(x<0){
+  print("es menor a cero")
+}else{}
+}
+}
+
+
+
---
+++ clase viernes 26.R
@@ -0,0 +1,6 @@
+#areaCirculo: floar->float
+#calcula el area de un circulo con radio "radio"
+#ejemplo:2->12.56
+areaCirculo<-function(radio){3.1415*radio^2}
+
+variableSiete<-areaCirculo(2)
